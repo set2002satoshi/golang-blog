222fa03259a5f4c9747369881b61d88a8dcfe7f3
田代智 <r03.web.2110258@da.iwasaki.ac.jp>
2022-06-20 13:07:34 +0900

    少しの間フロントは保留

---
+++ frontend/fornt-app/pages/register.tsx
@@ -0,0 +1,16 @@
+import React from 'react'; 
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import RegisterForm from '../components/certifications/RegisterForm'
+
+
+const Register: NextPage = () => {
+    return (
+        <div>
+            <RegisterForm />    
+        </div>
+    )
+
+}
+
+export default Register
